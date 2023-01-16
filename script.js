37e6746316d6a0035888f23de0f0b4ce12b73878
Arpan <arpanseal1234@gmail.com>
2023-01-16 21:10:03 +0530

    :)

---
+++ script.js
@@ -0,0 +1,510 @@
+const arr=[`Why did the chicken cross the playground?<br><br>
+To get to the other slide!`,
+
+`Why did the actor fall through the floorboards?<br><br>
+They were going through a stage!`,
+
+`Why did a scarecrow win a Nobel prize?<br><br>
+He was outstanding in his field!`,
+
+`Why are peppers the best at archery?<br><br>
+Because they habanero!`,
+
+`What did the duck say after she bought chapstick?<br><br>
+Put it on my bill!`,
+
+`Why did an old man fall in a well?<br><br>
+Because he couldn’t see that well!`,
+
+`What do you call a fake noodle?<br><br>
+An impasta!`,
+
+`What did the three-legged dog say when he walked into a saloon?<br><br>
+“I’m looking for the man who shot my paw!”`,
+
+`How do you tell the difference between a bull and a cow?<br><br>
+It is either one or the udder!`,
+
+`What’s red and smells like blue paint?<br><br>
+Red paint!`,
+
+`What’s the difference between a hippo and a Zippo?<br><br>
+One is very heavy, the other is a little lighter!`,
+
+`Why can’t you send a duck to space?<br><br>
+Because the bill would be astronomical!`,
+
+`What does Jeff Bezos do before he goes to sleep?<br><br>
+He puts his PJ-Amazon!`,
+
+`What happened when the world's tongue-twister champion got arrested?<br><br>
+They gave him a tough sentence!`,
+
+`What did the mama cow say to the calf?<br><br>
+It’s pasture bedtime!`,
+
+`How does a vampire start a letter?<br><br>
+Tomb it may concern!`,
+
+`What do you call an illegally parked frog?<br><br>
+Toad!`,
+
+`What did one plate say to the other?<br><br>
+Dinner is on me!`,
+
+`Why do hummingbirds hum?<br><br>
+Because they don’t know the words!`,
+
+`What do sprinters eat before a race?<br><br>
+Nothing. They fast!`,
+
+`Two muffins are baking in an oven. One of them looks to the other and says, "Phew, it's getting hot in here!" The other looks back and says, "Ack! A talking muffin!"`,
+
+`What do cats eat for breakfast?<br><br>
+Mice Crispies!`,
+
+`What do you call an elephant that doesn’t matter?<br><br>
+An irrelephant!`,
+
+`What do you get when you cross a rabbit with shellfish?<br><br>
+An oyster bunny!`,
+
+`Where do polar bears keep their money?<br><br>
+In a snow bank!`,
+
+`Why did the pony get sent to his room?<br><br>
+He wouldn’t stop horsing around!`,
+
+`What kind of dog does a magician have?<br><br>
+A Labracadabrador!`,
+
+`Where do cows go on Friday nights?<br><br>
+They go to the moo-vies!`,
+
+`Why couldn't the pony sing “Happy Birthday?<br><br>”
+Because she was just a little hoarse!`,
+
+`How do you make an octopus laugh?<br><br>
+With ten-tickles!`,
+
+`How do you keep a bull from charging?<br><br>
+Take away its credit card!`,
+
+`Why can’t a leopard hide?<br><br>
+Because he’s always spotted!`,
+
+`What do frogs order at fast-food restaurants?<br><br>
+French flies!`,
+
+`What do you get from a pampered cow?<br><br>
+Spoiled milk!`,
+
+`What do you call an alligator who solves mysteries?<br><br>
+An investigator!`,
+
+`Why is a snake difficult to fool?<br><br>
+You can’t pull its leg!`,
+
+`What kind of socks do grizzlies wear?<br><br>
+None, they have bear feet!`,
+
+`What do you get when you cross a snail with a porcupine?<br><br>
+A slowpoke!`,
+
+`What did the dog say when it sat on sandpaper?<br><br>
+"Ruff!"`,
+
+`What's a cat's favorite dessert?<br><br>
+Chocolate mouse!`,
+
+`What fish only swims at night?<br><br>
+Starfish!`,
+
+`What does a triceratops sit on?<br><br>
+Its tricera-bottom!`,
+
+`Knock, knock.
+Who’s there?<br><br>
+Weekend.
+Weekend who?<br><br>
+Weekend do anything we want!`,
+
+`Knock, knock.
+Who’s there?<br><br>
+Figs.
+Figs who?<br><br>
+Figs the doorbell, I’ve been knocking forever!`,
+
+`Knock, knock.
+Who’s there?<br><br>
+Theodore.
+Theodore who?<br><br>
+Theodore wasn’t opened so I knocked!`,
+
+`Knock, knock.
+Who’s there?<br><br>
+Tank.
+Tank who?<br><br>
+You’re welcome!`,
+
+`Knock, knock.
+Who’s there?<br><br>
+Isabel.
+Isabel who?<br><br>
+Isabel not working?<br><br>`,
+
+`Knock, knock.
+Who’s there?<br><br>
+Ice cream.
+Ice cream who?<br><br>
+ICE CREAM SO YOU CAN HEAR ME!`,
+
+`Knock, knock.
+Who’s there?<br><br>
+Icy.
+Icy who?<br><br>
+Icy you in there!`,
+
+`Knock, knock.
+Who’s there?<br><br>
+Dozen.
+Dozen who?<br><br>
+Dozen anyone want to let me in?<br><br>`,
+
+`Knock, knock.
+Who’s there?<br><br>
+Scold.
+Scold who?<br><br>
+Scold outside, let me in!`,
+
+`Knock, knock.
+Who’s there?<br><br>
+Water.
+Water who?<br><br>
+Water you asking so many questions for, just open up!`,
+
+`Knock, knock.
+Who’s there?<br><br>
+Cargo.
+Cargo who?<br><br>
+Car go, “Toot toot, vroom, vroom!”`,
+
+`Knock, knock.
+Who’s there?<br><br>
+Hatch.
+Hatch who?<br><br>
+Bless you!`,
+
+`Knock, knock.
+Who’s there?<br><br>
+Annie.
+Annie who?<br><br>
+Annie body home?<br><br>`,
+
+`Knock, knock.
+Who’s there?<br><br>
+Boo.
+Boo who?<br><br>
+Don’t cry, it’s just a joke!`,
+
+`Knock, knock.
+Who’s there?<br><br>
+Lettuce.
+Lettuce who?<br><br>
+Lettuce in, it’s cold out here!`,
+
+`Knock, knock.
+Who’s there?<br><br>
+An interrupting cow.
+An interrupt—
+MOO!`,
+
+`Knock, knock.
+Who’s there?<br><br>
+Owls go.
+Owls go who?<br><br>
+That’s right!`,
+
+`Knock, knock.
+Who’s there?<br><br>
+A little old lady.
+A little old lady who?<br><br>
+Hey, I didn’t know you could yodel!`,
+
+`Knock, knock.
+Who’s there?<br><br>
+Nana.
+Nana who?<br><br>
+Nana your business!`,
+
+`Knock, knock.
+Who’s there?<br><br>
+Toodle
+Toodle who?<br><br>
+Toodle-loo!`,
+
+`Knock, knock.
+Who’s there?<br><br>
+Spell.
+Spell who?<br><br>
+Okay, W-H-O!`,
+
+`What’s a piece of bread’s least favorite chore?<br><br>
+Doing a loaf of laundry.`,
+
+`What did the bunny say to the carrot?<br><br>
+It’s been nice gnawing you!`,
+
+`What do you call a sad strawberry?<br><br>
+A blueberry!`,
+
+`Why did the tomato blush?<br><br>
+Because it saw the salad dressing!`,
+
+`What did the pizza say to the topping?<br><br>
+I never sau-sage a pretty face!`,
+
+`Which vegetable do sailors hate the most?<br><br>
+Leeks!`,
+
+`What do you call a cheese that’s not yours?<br><br>
+Nacho cheese!`,
+
+`Why did the cookie go to the nurse?<br><br>
+Because he felt crummy!`,
+
+`What kind of room doesn’t have doors?<br><br>
+A mushroom!`,
+
+`What kind of key opens a banana?<br><br>
+A mon-key!`,
+
+`What happens when a grape gets run over crossing the street?<br><br>
+A traffic jam!`,
+
+`What does garlic do when it gets hot?<br><br>
+It takes its cloves off!`,
+
+`Where does fruit go on vacation?<br><br>
+Pear-is!`,
+
+`Why did the melons choose not to get married?<br><br>
+Because they cantaloupe!`,
+
+`What does a lemon say when it answers the phone?<br><br>
+Yellow!`,
+
+`What did one dried fruit say when another asked it to the movies?<br><br>
+It's a date!`,
+
+`Why do bees have sticky hair?<br><br>
+They use honeycombs!`,
+
+`What does a cow call an earthquake?<br><br>
+A milkshake!`,
+
+`Why did the man go to the yogurt museum?<br><br>
+To get a little culture!`,
+
+`Why couldn't the sesame seed climb up the hill?<br><br>
+Because it was on a roll!`,
+
+`Why can't you trust tacos?<br><br>
+Because they always spill the beans!`,
+
+`How come no one picks statistics as their favorite subject?<br><br>
+It’s just average.`,
+
+`Why did two 4s skip dinner?<br><br>
+Because they already 8!`,
+
+`Did you hear about the mathematician who’s afraid of negative numbers?<br><br>
+He’ll stop at nothing to avoid them!`,
+
+`What did the triangle feel sorry for the circle?<br><br>
+Because it’s pointless!`,
+
+`Why was the math book sad?<br><br>
+It had too many problems!`,
+
+`Did you hear about the rancher who had 97 cows in his field?<br><br>When he rounded them up, he had 100!`,
+
+`Why couldn’t the angle get a loan?<br><br>
+Because his parents wouldn’t cosine!`,
+
+`What do you call a number that can’t stay in one place?<br><br>
+A roamin’ numeral!`,
+
+`Why should you never trust someone writing on graph paper?<br><br>
+Because they must be plotting something!`,
+
+`What do you call two guys who love math?<br><br>
+Algebros!`,
+
+`What do you get when you divide a Jack o' lantern by its diameter?<br><br>
+Pumpkin pi!`,
+
+`What did the 90° angle say after an argument?<br><br>
+"It turns out, I was right!"`,
+
+`What tool do mathematicians use most?<br><br>
+Multi-pliers!`,
+
+`Why did the student get upset when their teacher called them average?<br><br>
+It was a mean thing to say!`,
+
+`If a math teacher had four apples in one hand and five apples in the other hand, what would they have altogether?<br><br>
+Really big hands!`,
+
+`What did the 0 say to the 8?<br><br>
+Nice belt!`,
+
+`Why was 6 afraid of 7?<br><br>
+Because 7, 8, 9!`,
+
+`Why should you never start a conversation with pi?<br><br>
+It'll just go on forever!`,
+
+`What is a bird’s favorite type of math?<br><br>
+OWL-gebra!`,
+
+`Why do teenagers travel in groups of threes and fives?<br><br>
+Because they can’t even!`,
+
+`Did you hear about the the circle that kept going to school?<br><br>
+It has 360°!`,
+
+`Why did the robber take a bath before he left the scene of the crime?<br><br>
+He wanted to make a clean getaway!`,
+
+`Why does it take pirates a long time to learn the alphabet?<br><br>
+Because they can spend years at C!`,
+
+`What did one snowman say to the other snowman?<br><br>
+It smells like carrots over here!`,
+
+`What did one wall say to the other wall?<br><br>
+I’ll meet you at the corner!`,
+
+`Why did the kid throw his clock out the window?<br><br>
+Because he wanted to see time fly!`,
+
+`What do you call a huge pile of cats?<br><br>
+A meow-ntain!`,
+
+`Why wouldn’t the shrimp share his treasure?<br><br>
+Because he was a little shellfish!`,
+
+`Why is Cinderella bad at soccer?<br><br>
+Because she’s always running away from the ball!`,
+
+`What does a rain cloud wear under its clothes?<br><br>
+Thunderwear!`,
+
+`Why do vampires seem sick all the time?<br><br>
+Because they’re always coffin!`,
+
+`How do you get an astronaut’s baby to stop crying?<br><br>
+You rocket!`,
+
+`Why are ghosts bad liars?<br><br>
+Because you can see right through them!`,
+
+`Why are spiders so smart?<br><br>
+They can find everything on the web!`,
+
+`What are mummies' favorite lunches?<br><br>
+Wraps!`,
+
+`How does Darth Vader like his toast?<br><br>
+On the dark side!`,
+
+`Why are mountains so funny?<br><br>
+They're just hill areas!`,
+
+`Why did the student eat his homework?<br><br>Because his teacher told him it would be a piece of cake!`,
+
+`What is the Easter bunny’s favorite type of music?<br><br>
+Hip-hop!`,
+
+`Why did Beethoven get rid of his chickens?<br><br>
+All they ever said was, “Bach, Bach, Bach!”`,
+
+`Why did the man name his dogs Rolex and Timex?<br><br>
+Because they were watchdogs!`,
+
+`Why is grass so dangerous?<br><br>
+Because it’s full of blades!`,
+
+`What did the teacher say when a book fell on their head?<br><br>
+I have only my shelf to blame.`,
+
+`What kind of tree can fit in one hand?<br><br>
+A palm tree!`,
+
+`How do they keep the basketball arena cool?<br><br>
+They fill it with fans!`,
+
+`What does a book do to keep warm in the winter?<br><br>
+It puts on a jacket!`,
+
+`What did the lightbulb say to its sweetheart?<br><br>
+I wuv you a watt!`,
+
+`What’s the difference between a guitar and a fish?<br><br>You can tune a guitar, but you can’t tuna fish!`,
+
+`What did the fisherman say to the magician?<br><br>
+Pick a cod, any cod!`,
+
+`Why did the picture go to prison?<br><br>
+Because it was framed!`,
+
+`Why do bicycles fall over?<br><br>
+Because they’re two-tired!`,
+
+`If a seagull flies over the sea, what flies over the bay?<br><br>
+A bagel!`,
+
+`What does the ocean do when it sees its friends?<br><br>
+It waves!`,
+
+`Why did the golfer wear two pairs of pants?<br><br>
+Just in case he got a hole in one!`,
+
+`Why was the broom late?<br><br>
+It over-swept!`,
+
+`What did the paper say to the pencil?<br><br>
+Write on!`,
+
+`What do you call a belt made of watches?<br><br>
+A waist of time!`,
+
+`Where do sailboats go when they're sick?<br><br>
+To the dock!`,
+
+`How does the moon cut his hair?<br><br>
+Eclipse it!`,
+
+`What do you do when a lemon gets sick?<br><br>
+You give it lemon-aid!`,
+
+`What’s worse than raining cats and dogs?<br><br>
+Hailing taxis!`,
+
+`How can you tell if a tree is a dogwood tree?<br><br>
+By its bark!`,
+
+`What do astronauts do before throwing a party?<br><br>
+They planet!`]
+
+const bgimgC=["https://wallpaperset.com/w/full/f/1/9/526346.jpg", "https://i.pinimg.com/originals/ec/29/85/ec298529c2a681dfdf2476c759c8af29.jpg", "https://wallpapercave.com/wp/wp9897671.jpg", "https://c4.wallpaperflare.com/wallpaper/969/153/168/3d-view-black-white-animals-funny-sheep-1680x1050-entertainment-funny-hd-art-wallpaper-preview.jpg", "https://wallpaperset.com/w/full/e/9/9/144745.jpg", "https://wallpapers.com/images/file/oversized-shiro-and-shin-chan-cartoon-v9cpao5ysh8m2fzm.jpg"]
+
+const bgimg = Math.floor(Math.random() * bgimgC.length);
+const rand = Math.floor(Math.random() * arr.length);
+let sl = document.getElementById("styleHTML");
+sl.innerHTML = `body{background: url(${bgimgC[bgimg]}) no-repeat center center/cover;`;
+
+// console.log(arr[rand]);
+let el = document.getElementById("para");
+el.innerHTML += `<div><p id="joke">${arr[rand]}</p></div>`;
